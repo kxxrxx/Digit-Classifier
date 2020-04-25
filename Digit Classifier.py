af0459fe1d11c699afb93659b3fed7b9e7e847f8
Lei <keishadaco@gmail.com>
2020-04-25 16:23:18 -0700

    Final
    Included the final version of the code and an image of the output

--- Digit Classifier.py
+++ Digit Classifier.py
@@ -10,8 +10,7 @@ import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
-from keras.layers.normalization import BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 import time
 
@@ -19,7 +18,7 @@ start = time.time()
 
 batch_size = 128 # 128 images trained at a time
 num_classes = 10 # 0 to 9 = 10 classes
-epochs = 2
+epochs = 12
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -62,31 +61,21 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 # y_train[0] = [0., 0., 0., 0., 0., 1., 0., 0., 0., 0.] for #5
 
 model = Sequential()
-ZeroPadding2D((2,2))
-model.add(Conv2D(64, kernel_size=(5, 5), # 32 filters, kernel = size of filter,
+model.add(Conv2D(32, kernel_size=(3, 3), # 32 filters, kernel = size of filter,
                  activation='relu', # rectified linear unit
                  # f(x) = max(0,x), sets neg vals to 0, constant input x 
                  input_shape=input_shape)) # input shape for first layer
-ZeroPadding2D((2,2))
-model.add(Conv2D(128, (3, 3), activation='relu',padding='same'))
-model.add(MaxPooling2D(pool_size=(3, 3),strides=(2,2)))
-ZeroPadding2D((2,2))
-model.add(Conv2D(256, (3, 3), activation='relu'))
-ZeroPadding2D((1,1))
-model.add(Conv2D(256, (5, 5), activation='relu',padding='same'))
-model.add(MaxPooling2D(strides=(2,2)))
-model.add(Dropout(0.2))
-ZeroPadding2D((1,1))
-model.add(Conv2D(512, (3, 3), activation='relu'))
-model.add(Dropout(0.2))
-ZeroPadding2D((1,1))
-model.add(Conv2D(512, (5, 5), activation='relu',padding='same'))
-model.add(MaxPooling2D(strides=(2,2))) # downsamples the input
-#model.add(Dropout(0.4)) # randomly disables 25% of neurons (reduces overfitting)
+model.add(Conv2D(32, (5, 5), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(3, 3))) # downsamples the input
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (5, 5), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(3, 3))) # downsamples the input
+model.add(Dropout(0.4)) # randomly disables 40% of neurons (reduces overfitting)
 model.add(Flatten()) # flattens the 2D arrays for fully connected layers
-model.add(Dropout(0.5))
-model.add(Dense(2048, activation='relu')) # batch size = 128 = hidden neuron units
-model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(128, activation='relu')) # 128 = hidden neuron units
+model.add(Dropout(0.4))
+model.add(Dense(num_classes, activation='softmax')) # num_classes = 10, units in output layer
+# outputs max probability
 
 model.compile(loss=keras.losses.categorical_crossentropy, # for multi-classification (>2, binary = 2)
               optimizer=keras.optimizers.Nadam(), # learning rate = 1, rho = 0.95
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# MNIST
+This project utilizes the Tensorflow framework and Keras library to develop a convolutional neural network (CNN) able to recognize handwritten digits recorded in the MNIST dataset. The MNIST dataset comprises 60,000 training samples and 10,000 test samples of handwritten digits, 0-9. The goal of this project is to maximize accuracy while minimizing loss and runtime. 
+
+A convolutional neural network (CNN) is designed and implemented to classify digits by applying filters to formatted 28x28 pixel monochrome images to extract and learn higher-level features. It is composed of a series of convolutional layers, pooling layers, and dense layers. The CNN is first trained on the 60000-image training dataset and then tested using the 10000-image testing set for validation. 
+
+In this project, I will experiment with adjusting various parameters and documenting their effects. Following the process dictated, I achieved a maximum accuracy of 99.59%, testing loss of 0.019198, and a final runtime of 252 seconds in the 12th epoch for a total execution time of 3059.848 seconds..
+
  
